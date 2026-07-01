60c1d7433a2e93b1deae28b1a7b440d3e82c5ec5
eo404 <eldhox010@gmail.com>
2026-07-01 11:20:58 +0530

    frontend implementation started

---
+++ jobTracker-client/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App(){
+  return(
+    <div>
+      <h1>Job Tracker</h1>
+      <p>Welcome to my first react application</p>
+    </div>
+  );
+}
+
+export default App
